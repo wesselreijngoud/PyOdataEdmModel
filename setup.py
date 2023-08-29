f934da53f2d98b04af7bc1a4bcc3c9fa34e1d21e
wesselreijngoud <w.reijngoud@student.rug.nl>
2023-08-29 15:20:15 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='PyOdataEdmModel',
     version='1.0.0',
+    author='Wessel Reijngoud',
+    author_email='wreijngoud@ilionx.com',
+    keywords='odata edmmodel metadata',
     description='The ODataEdmBuilder is a Python class that facilitates the construction of an Entity Data Model (EDM) for OData services.',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=['pandas==2.0.3'],
 )
