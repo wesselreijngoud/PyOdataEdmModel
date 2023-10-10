0a591a461015222cee329d9738c410ae2ddf1982
Wessel Reijngoud <wesselreijn@live.nl>
2023-10-10 08:51:19 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -2,13 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name="PyOdataEdmModel",
-    version="1.0.0",
+    version="1.0.2",
     author="Wessel Reijngoud",
     author_email="wreijngoud@ilionx.com",
     keywords="odata edmmodel metadata",
     description="The ODataEdmBuilder is a Python class that facilitates the construction of an Entity Data Model (EDM) for OData services.",
     packages=find_packages(),
-    data_files=[('config', ['PyOdataEdmModel/config/template.json', 'PyOdataEdmModel/config/settings.json'])],
+    data_files=[
+        (
+            "config",
+            [
+                "PyOdataEdmModel/config/template.json",
+                "PyOdataEdmModel/config/settings.json",
+            ],
+        )
+    ],
     include_package_data=True,
-    install_requires=["pandas==2.0.3"],
+    install_requires=["pandas==2.1.1"],
+    project_urls={"Source": "https://github.com/wesselreijngoud/PyOdataEdmModel"},
 )
