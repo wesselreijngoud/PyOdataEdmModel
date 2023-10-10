36c8c66bd1a2adb00db3da4fc6bc0c4c26f124c4
Wessel Reijngoud <wesselreijn@live.nl>
2023-10-10 09:08:37 +0200

    added long description and upped version num

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="PyOdataEdmModel",
-    version="1.0.3",
+    version="1.0.4",
     author="Wessel Reijngoud",
     author_email="wreijngoud@ilionx.com",
     keywords="odata edmmodel metadata",
     description="The ODataEdmBuilder is a Python class that facilitates the construction of an Entity Data Model (EDM) for OData services.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     data_files=[
         (
